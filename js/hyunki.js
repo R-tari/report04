d504e4677e370ebf6127ce17cd36a6cfad5b03f5
R-tari <95198393+R-tari@users.noreply.github.com>
2022-01-19 14:46:06 +0900

    Add files via upload

---
+++ js/hyunki.js
@@ -0,0 +1,29 @@
+$(function()
+      {
+
+        $('.slider').bxSlider();
+       
+          $('.btn-nav').on(
+              {
+                  'click':function()
+                  {
+                      $('.nav').toggleClass('show-nav');
+                      $('.nav li').toggleClass('show-nav');
+                  }
+              }
+          );
+
+          $('.nav li').on(
+              {
+                  'click':function()
+                  {
+                      $('.nav').removeClass('show-nav');
+                      $('.nav li').removeClass('show-nav');
+                  }
+              }
+          )
+
+              
+         
+   
+      })
